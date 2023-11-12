d35303f6ada9aa1f5ba23a86ff33c486b74b25f3
gzavertyaev-dev <149540154+gzavertyaev-dev@users.noreply.github.com>
2023-11-12 07:01:17 +0100

    Create elements.js

---
+++ elements.js
@@ -0,0 +1,15 @@
+// Элементы управления
+export const SEARCH_BUTTON = document.querySelector('#search-button');
+export const SEARCH_CITY_INPUT = document.querySelector('#city-input');
+
+// Контейнеры для отображения
+export const LOADING_TEXT = document.querySelector('#load');
+export const WEATHER_INFO_CONTAINER = document.querySelector('#weather-info-container');
+export const ERROR_INFO_CONTAINER = document.querySelector('#error-container')
+
+// Тексты с информацией
+export const WEATHER_CITY = document.querySelector('#weather-city');
+// export const WEATHER_DESCRIPTION = document.querySelector('#weather-description');
+// export const WEATHER_TEMPERATURE = document.querySelector('#weather-temperature');
+export const ERROR_MESSAGE = document.querySelector('#error-message');
+
